ed050f5314121e14372d0fd7d0c2bdeea1fb04c3
Khoi HOANG <khoi.hoang@exwzd.com>
2021-12-06 18:10:04 +0900

    Removed unecessary type and added more tags

--- frontend/domain/models/Tag.ts
+++ frontend/domain/models/Tag.ts
@@ -2,4 +2,7 @@ export enum Tag {
   YOUTH = 'youth',
   PRAYER = 'prayer',
   HYMN = 'hymn',
+  PASTORAL_LETTER = 'pastoral_letter',
+  APOSTOLIC_EXHORTATION = 'apostolic_exhortation',
+  FAITH = 'faith',
 }
